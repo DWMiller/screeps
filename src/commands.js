e99c90dea281cda8c957f0910e0d0961441d850e
David Miller <davidabc66@gmail.com>
2016-09-25 21:50:29 -0400

    minor reformatting

--- src/commands.js
+++ src/commands.js
@@ -16,7 +16,9 @@ Game.spawns.Spawn1.createCreep([WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE],
         role: 'harvester'
     });
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
+var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+console.log('Harvesters: ' + harvesters.length);
 
-      var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+var newName = Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {
+    role: 'harvester'
+});
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
-var roleUpgrader = require('role.upgrader');
+var roleHarvester = require('role.harvester')();
+var roleUpgrader = require('role.upgrader')();
+var roleBuilder = require('role.builder')();
 
 module.exports.loop = function() {
 
